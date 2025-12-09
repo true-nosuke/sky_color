6af8950af8d73bc516bb3ecde5a088f5404d46d8
true-nosuke <nosuke9002@icloud.com>
2025-12-09 14:45:23 +0900

    Firebase SDKをインポートし、スクリプトのモジュールタイプを設定

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBr7hYG63QrWjxB1BqNFxZ9qSMTuX9TFU4",
+    authDomain: "sky-color-japan.firebaseapp.com",
+    projectId: "sky-color-japan",
+    storageBucket: "sky-color-japan.firebasestorage.app",
+    messagingSenderId: "68931988312",
+    appId: "1:68931988312:web:67c92fbc1664bbe889d32d",
+    measurementId: "G-N14PGM1TK6"
+  };
 document.addEventListener('DOMContentLoaded', () => {
     // --- 定数・設定 ---
     const STORAGE_KEY_POSTS = 'sky_color_posts';
